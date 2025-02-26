0f1247afa66127c35ac8629ac5e96c5250f9d5e7
Hoang Duc <minhduchoang2303@gmail.com>
2025-02-26 20:04:56 +0700

    bai tap 11.1

---
+++ BT11_1_toantu/baitap4.js
@@ -0,0 +1,4 @@
+let Pi = Math.PI;
+let r = prompt("Nhap ban kinh duong tron= ");
+console.log("CHU VI LA:", 2 * Pi * r);
+console.log("DIEN TICH LA:", Pi * r * r);
---
+++ BT11_1_toantu/baitap5.js
@@ -0,0 +1,4 @@
+let a = prompt("Nhap chieu dai: ");
+let b = prompt("nhap chieu rong: ");
+console.log("Chu vi la:", (a + b) * 2);
+console.log("Chu vi la:", a * b);
